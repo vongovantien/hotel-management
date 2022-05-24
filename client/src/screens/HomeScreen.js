4446a1041fe38144db7b8e858e34f29b4704e322
Tiền Võ <tienvo@meu-solutions.com>
2022-05-24 10:54:13 +0700

    update

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,16 +1,13 @@
 import React from "react";
-import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header.js";
-import CallToActionSection from "../components/Home/CallToActionSection.js";
-import ContactInfo from "../components/Home/ContactInfo.js";
-import ShopSection from "../components/Home/ShopSection.js";
+// import ContactInfo from "../components/Home/ContactInfo.js";
+// import ShopSection from "../components/Home/ShopSection.js";
 
 const HomeScreen = () => {
     return (
         <div>
-            <ShopSection />
+            {/* <ShopSection />
             <CallToActionSection />
-            <ContactInfo />
+            <ContactInfo /> */}
         </div>
     );
 };
